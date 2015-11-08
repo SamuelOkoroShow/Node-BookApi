09893a44b69d52dcda5efd94cf495a71fcf0a5af
Sam O <sam@samokoro.com>
2015-11-08 18:13:02 -0500

    Express Router & Json return

--- app.js
+++ app.js
@@ -3,13 +3,29 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 2000;
 
+var bookRouter = express.Router();
+
+bookRouter.route('/Books')
+    .get(function(req, res){
+        var responseJson = {
+            hello: "This is my API"
+        }
+        res.json(responseJson);
+    });
+
+
+
+app.use('/api', bookRouter);
+
+
+
+
 app.get('/', function(req, res){
-res.send('welcome to my API');
+    res.send('welcome to my API');
 
 });
 
 
-app.listen(port,function(){
-console.log('works on :' + port);
+app.listen(port, function(){
+    console.log('works on :' + port);
 });
-
